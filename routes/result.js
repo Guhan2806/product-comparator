cb8da9273dbb5e0267c135d73e151657bdebba56
Guhan2806 <guhan4410@gmail.com>
2022-10-10 22:09:51 +0530

    Server Side data sent

--- routes/result.js
+++ routes/result.js
@@ -3,33 +3,44 @@ const request = require('request');
 const cheerio = require('cheerio');
 const router =express.Router()
 var flip_res=[]
-    var title=''
-      var price=''
-      var img=''
-      var link=''
-      var f_link=''
-var amaz_res={}
+var amaz_res=[]
 function scrap_flipkart(result){
-    
-    request('https://www.flipkart.com/search?q=poco+m4', (error, response, html) => {
+    flip_res.length=0
+    var temp=result.split(" ")
+    var flip_link= 'https://www.flipkart.com/search?q='
+    flip_link=flip_link+temp[0]
+    for(var i=1;i<temp.length;i++)
+    {
+      flip_link=flip_link+'+'+temp[i]
+    }
+    request(flip_link, (error, response, html) => {
   if (!error && response.statusCode == 200) {
     const $ = cheerio.load(html);
     $('._2kHMtA').each((i,el)=>{
-       title=$(el).find('._4rR01T').html()
-       price=$(el).find('._30jeq3').html()
-       img=$(el).find('._396cs4._3exPp9').attr().src
-       link=$(el).find('._1fQZEK').attr().href
-             f_link='https://www.flipkart.com'+link
-      flip_res.push({title:title,price:price,img:img,f_link:f_link})
+       const title=$(el).find('._4rR01T').html()
+       const price=$(el).find('._30jeq3').html()
+       const img=$(el).find('._396cs4._3exPp9').attr().src
+       const link=$(el).find('._1fQZEK').attr().href
+       const f_link='https://www.flipkart.com'+link
+      flip_res.push({title,price,img,f_link})
     })
-    return flip_res[0]
+    console.log(flip_res)
+    return flip_res
     }
-}
+  }
 )
+return flip_res
 }
 function scrap_amazon(result)
 {
-    request('https://www.amazon.in/s?k=poco+m4+pro', (error, response, html) => {
+  var temp=result.split(" ")
+    var amaz_link= 'https://www.amazon.in/s?k='
+    amaz_link=amaz_link+temp[0]
+    for(var i=1;i<temp.length;i++)
+    {
+      amaz_link=amaz_link+'+'+temp[i]
+    }
+    request(amaz_link, (error, response, html) => {
   if (!error && response.statusCode == 200) {
     const $ = cheerio.load(html);
     $('.s-card-container.s-overflow-hidden.aok-relative.puis-include-content-margin.s-latency-cf-section.s-card-border').each((i,el)=>{
@@ -44,14 +55,21 @@ function scrap_amazon(result)
         a_link='https://www.amazon.in'+link.href
       }
       if(title!=null &&price!=null)
-      console.log(title,price,img,a_link)
+      amaz_res.push({title,price,img,a_link})
     })
+    return amaz_res
     }
+    return amaz_res
 });
+return amaz_res
 }
 router.post('/result',(req,res)=>{
     const result=req.body.res
-    const summa=scrap_flipkart(result)
-    console.log(summa)
+    const f_res=scrap_flipkart(result)
+    const a_res=scrap_amazon(result)
+    console.log(f_res)
+    res.json({flipkart:f_res,amazon:a_res})
+    amaz_res.length=0
+    flip_res.length=0
 })
 module.exports=router
